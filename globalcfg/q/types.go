7110f1f4060c36d7bff32250d2228dad0295ae22
zytyan <58136520+zytyan@users.noreply.github.com>
2025-11-22 13:55:29 +0800

    修改Unixtime为值接收者

--- globalcfg/q/types.go
+++ globalcfg/q/types.go
@@ -4,28 +4,55 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
+	"strconv"
 	"time"
 )
 
 type UnixTime struct {
-	time.Time
+    time.Time
 }
 
 func (u *UnixTime) Scan(value any) error {
-	switch val := value.(type) {
-	case int64:
-		u.Time = time.Unix(val, 0)
-		return nil
-	case float64:
-		u.Time = time.Unix(int64(val), 0)
-		return nil
-	default:
-		return fmt.Errorf("cannot convert %v of type %T to UnixTime", value, value)
-	}
+    switch val := value.(type) {
+    case int64:
+        // SQLite INTEGER
+        u.Time = time.Unix(val, 0)
+        return nil
+
+    case float64:
+        // JSON float → SQLC sometimes gives float
+        u.Time = time.Unix(int64(val), 0)
+        return nil
+
+    case time.Time:
+        // MySQL / Postgres
+        u.Time = val
+        return nil
+
+    case []byte:
+        // SQLite sometimes returns []byte for INTEGER
+        i, err := strconv.ParseInt(string(val), 10, 64)
+        if err != nil {
+            return err
+        }
+        u.Time = time.Unix(i, 0)
+        return nil
+
+    case string:
+        // If database stores as string timestamp
+        i, err := strconv.ParseInt(val, 10, 64)
+        if err != nil {
+            return err
+        }
+        u.Time = time.Unix(i, 0)
+        return nil
+    }
+
+    return fmt.Errorf("cannot convert %v (%T) to UnixTime", value, value)
 }
 
-func (u *UnixTime) Value() (driver.Value, error) {
-	return u.Unix(), nil
+func (u UnixTime) Value() (driver.Value, error) {
+    return u.Unix(), nil
 }
 
 type ChatCfg struct {
