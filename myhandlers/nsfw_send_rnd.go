94ab275462bdca9a8bd18001bee8792404c0f9c8
zytyan <58136520+zytyan@users.noreply.github.com>
2025-11-23 16:41:52 +0800

    feat: 允许用户要求随机等级的涩图

--- myhandlers/nsfw_send_rnd.go
+++ myhandlers/nsfw_send_rnd.go
@@ -1,6 +1,8 @@
 package myhandlers
 
 import (
+	"context"
+	g "main/globalcfg"
 	"main/globalcfg/h"
 	"regexp"
 
@@ -8,26 +10,21 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
-func SendRandAdult(bot *gotgbot.Bot, ctx *ext.Context) error {
-	photo := getRandomNsfwAdult()
-	if photo == "" {
-		_, err := ctx.EffectiveMessage.Reply(bot, "没有色图~", nil)
-		return err
-	}
-	_, err := bot.SendPhoto(ctx.EffectiveChat.Id, gotgbot.InputFileByID(photo), &gotgbot.SendPhotoOpts{
-		ReplyParameters: MakeReplyToMsgID(ctx.EffectiveMessage.MessageId),
-		HasSpoiler:      true,
-	})
-	return err
-}
+var reRank = regexp.MustCompile(`(\d)(\b\d)?`)
 
 func SendRandRacy(bot *gotgbot.Bot, ctx *ext.Context) error {
-	photo := getRandomNsfwRacy()
+	submatch := reRank.FindStringSubmatch(ctx.Message.Text)
+	start := defaultAtoi(submatch[0], 2)
+	end := defaultAtoi(submatch[1], start)
+	photo, err := g.Q.GetPicByUserRateRange(context.Background(), start, end)
+	if err != nil {
+		return err
+	}
 	if photo == "" {
 		_, err := ctx.EffectiveMessage.Reply(bot, "没有涩图~", nil)
 		return err
 	}
-	_, err := bot.SendPhoto(ctx.EffectiveChat.Id, gotgbot.InputFileByID(photo), &gotgbot.SendPhotoOpts{
+	_, err = bot.SendPhoto(ctx.EffectiveChat.Id, gotgbot.InputFileByID(photo), &gotgbot.SendPhotoOpts{
 		ReplyParameters: MakeReplyToMsgID(ctx.EffectiveMessage.MessageId),
 	})
 	return err
@@ -35,17 +32,7 @@ func SendRandRacy(bot *gotgbot.Bot, ctx *ext.Context) error {
 
 var reRacyPattern = regexp.MustCompile(`来[张点个]([涩色瑟]|(se))图|再来一张|來[張點個]([澀色瑟]|(se))圖`)
 
-func IsRequiredAdult(msg *gotgbot.Message) bool {
-	if len(msg.Text) == 0 {
-		return false
-	}
-	if !h.ChatRespNsfwMsg(msg.Chat.Id) {
-		return false
-	}
-	return false
-}
-
-func IsRequiredRacy(msg *gotgbot.Message) bool {
+func RequireNsfw(msg *gotgbot.Message) bool {
 	if len(msg.Text) == 0 {
 		return false
 	}
