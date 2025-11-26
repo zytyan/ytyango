0715dcfaa66783009b637d1f644d19d389811ae0
zytyan <58136520+zytyan@users.noreply.github.com>
2025-11-26 22:02:26 +0800

    WIP: 允许使用公网服务器进行测试

--- http/frontend/vite.config.ts
+++ http/frontend/vite.config.ts
@@ -1,15 +1,17 @@
 // vite.config.ts
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import process from "node:process";
 
+const allowedHost = process.env.VITE_ALLOWED_HOST
 export default defineConfig({
     plugins: [vue()],
+    server: {
+        allowedHosts: true
+    },
     build: {
         // 1️⃣ 顶层启用 SSR sourcemap
         sourcemap: true,
-        server: {
-            allowedHosts: true
-        },
         // 2️⃣ 明确告知 SSR 构建也要 sourcemap
         rollupOptions: {
             output: {
