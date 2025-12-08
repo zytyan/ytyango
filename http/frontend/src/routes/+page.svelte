c2393bf435abddbb5f146b2c0f46b76de2cf3aac
zytyan <58136520+zytyan@users.noreply.github.com>
2025-12-08 23:15:54 +0800

    chore: add svelte frontend scaffold

---
+++ http/frontend/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Avatar from "$lib/Avatar.svelte";
+    import SearchBar from "$lib/SearchBar.svelte";
+    function onSearch(query: string){
+        console.log(query);
+    }
+</script>
+
+<SearchBar {onSearch}></SearchBar>
+<Avatar src="https://i.pravatar.cc/300" name="Zest" fallbackKey="f12w23" />
