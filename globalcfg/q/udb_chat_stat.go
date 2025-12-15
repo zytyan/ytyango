0471b2cff665acd9ade89552f27f11e36aee6986
zytyan <58136520+zytyan@users.noreply.github.com>
2025-12-16 01:36:18 +0800

    feat: 更新统计功能

--- globalcfg/q/udb_chat_stat.go
+++ globalcfg/q/udb_chat_stat.go
@@ -9,8 +9,6 @@ import (
 	"main/helpers/lrusf"
 	"sync"
 	"time"
-
-	"go.uber.org/zap"
 )
 
 const (
@@ -226,9 +224,7 @@ var chatStatCache *lrusf.Cache[ChatStatKey, *ChatStat]
 // It is safe to call concurrently with other stat operations.
 func (q *Queries) FlushChatStats(ctx context.Context) error {
 	var firstErr error
-	q.logger.Info("flushing chat_stats")
 	for _, stat := range chatStatCache.Range() {
-		q.logger.Info("flushing", zap.Any("stat", stat))
 		if stat == nil {
 			continue
 		}
