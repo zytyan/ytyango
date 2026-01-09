64ca0e9cf186fa4daaaf83b4acc6ff7bf46011ad
zytyan <58136520+zytyan@users.noreply.github.com>
2026-01-10 01:10:13 +0800

    feat: 增加自定义系统提示词的帮助

--- handlers/gemini_ai.go
+++ handlers/gemini_ai.go
@@ -554,7 +554,18 @@ func UpdateGeminiSysPrompt(bot *gotgbot.Bot, ctx *ext.Context) error {
 				rawPrompt = "当前提示词为" + rawPrompt
 			}
 			_, err = msg.Reply(bot, "没有找到任何System prompt，请使用 /sysprompt 提示词或使用该命令回复其他消息设置提示词。\n"+
-				"使用 /resetsysprompt 恢复原始提示词\n"+rawPrompt, nil)
+				"使用 /resetsysprompt 恢复原始提示词"+
+				`你可以通过 %VAR% 使用变量，它会自动替换变量名，可使用的变量如下。
+TIME: 当前时间，不包含日期
+DATE: 当前日期，不含时间
+DATETIME: 当前时间和日期
+DATETIME_TZ: 包含时区的时间和日期
+CHAT_NAME: 当前聊天的名称
+BOT_NAME: Bot的名字
+BOT_USERNAME: Bot的username
+CHAT_TYPE: 聊天类型(group, private)
+例：现在是%DATETIME%，当前聊天为%CHAT_NAME%，请根据需要解答群友的问题。
+`+rawPrompt, nil)
 			return err
 		}
 	}
